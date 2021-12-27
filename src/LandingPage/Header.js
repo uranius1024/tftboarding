b28472bb85b2b7461c231910bedd9ea42b5eb134
Quyen <luongtrongq@gmail.com>
2021-12-27 18:33:36 +0700

    Update: HomePage

--- src/LandingPage/Header.js
+++ src/LandingPage/Header.js
@@ -1,10 +1,30 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import "../css/main.css"
 export default class Header extends Component {
     render() {
         return (
-            <div className=''>
-
+            <div className='HeadOutLine'>
+                <div className='InterOutLine container'>
+                    <div className='row'>
+                        <div className='HeadLogo col-md-6'>
+                            <img src='../images/logo.png' />
+                        </div>
+                        <div className='HeadMenu col-md-6'>
+                            <ul className='HeadList textGeneral'>
+                                <li>
+                                    <a href='#'>Home</a></li>
+                                <li>
+                                    <a href='#'>About</a></li>
+                                <li>
+                                    <a href='#'>Portfolio</a></li>
+                                <li>
+                                    <a href='#'>Blog</a></li>
+                                <li>
+                                    <a href='#'>Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Header from './Header'
 import HomePage from './HomePage'
 
 export default class LandingPage extends Component {
     render() {
         return (
             <div>
+                <Header />
                 <HomePage />
             </div>
         )
