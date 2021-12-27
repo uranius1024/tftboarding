4c0b1ed80a2ee9f00cac54efef0d495f8665b594
Quyen <luongtrongq@gmail.com>
2021-12-27 15:27:12 +0700

    Feat: Init Branch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import LandingPage from './LandingPage/LandingPage';
+function App() {
+  return (
+    <div >
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
